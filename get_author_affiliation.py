ca54b168f4df71296f9a453d74d1695e974e0c99
Eric Silberberg <72359394+esilberberg@users.noreply.github.com>
2022-05-17 19:06:33 -0400

    make dataframe for export to csv

--- get_author_affiliation.py
+++ get_author_affiliation.py
@@ -7,12 +7,16 @@ cuny_keywords = ['CUNY', 'City University of New York', 'Bronx', 'Lehman', 'Quee
 'Labor and Urban Studies', 'School of Law', 'School of Professional Studies', 'Guttman',
 'Hostos', 'Hunter', 'John Jay', 'LaGuardia', 'Medgar Evers', 'College of Technology', 'City College', 'York']
 
-df = pd.read_csv(r'C:\Users\erics\Desktop\SoTL-bibliography.csv')
+df = pd.read_csv('SoTL-bibliography.csv')
+
+# Start list of author information from Crossref
+authors = []
 
 # Get DOIs from CSV file
-for d in df['DOI']:
-    if isinstance(d, str):
-        doi = d
+for doi in df['DOI']:
+    if isinstance(doi, str):
+        row = []
+        row.append(doi)
         print(doi)
 
         # Create query to Crossref API
@@ -42,15 +46,28 @@ for d in df['DOI']:
                 # Test if the author is affiliated with CUNY
                 if num_of_authors > 1:
                     if any(word in affiliation for word in cuny_keywords):
+                        row.append(full_name)
+                        row.append(affiliation)
                         print(full_name)
                         print(affiliation)
                 else:
+                    row.append(full_name)
+                    row.append(affiliation)
                     print(full_name)
                     print(affiliation)
 
         except TypeError:
+            row.append('Authors unknown in Crossref')
             print('Authors unknown in Crossref')
         except KeyError:
+            row.append('Authors unknown in Crossref')
             print('Authors unknown in Crossref')
     else:
-        print('DOI not provided--------------------------')
+        row.append('DOI not provided')
+        print('DOI not provided')
+
+    authors.append(row)
+
+#Print to CSV
+df_authors = pd.DataFrame(authors)
+df_authors.to_csv('out.csv', encoding='utf-8-sig')
